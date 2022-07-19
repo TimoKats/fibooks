96d24313bfa94f43faf4ac55277ba2221ca6092a
Timo Kats <31708538+TimoKats@users.noreply.github.com>
2022-07-19 09:48:51 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup, find_packages
 
 classifiers = [
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Financial and Insurance Industry',
     'License :: OSI Approved :: MIT License',
     'Natural Language :: English',
-    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'Operating System :: Microsoft :: Windows :: Windows 11',
     'Programming Language :: Python :: 3'
 ]
 
 setup(
-
     name='fibooks',
-    version='1.0.3',
-    description='Python library for financial statement analysis. The full documentation can be found at: https://github.com/TimoKats/fibooks',
-    long_description='Python library for financial statement analysis. The full documentation can be found at: https://github.com/TimoKats/fibooks',
-    author='Timo Kats',
-    author_email='tpakats@gmail.com',
+    version='2.0.2',
+    packages=['fibooks'],
+    include_package_data=True,
+    url='https://github.com/TimoKats/fibooks/',
     license='MIT',
-    classifiers=classifiers,
-    keywords='finance',
-    packages=['fibooks', 'fibooks.classes'],
-    install_requires=['xlsxwriter']
-
+    author ='Timo Kats',
+    author_email='tpakats@gmail.com',
+    description='Python library for financial statement analysis. More information availible at: https://github.com/TimoKats/fibooks/',
+    long_description='Python library for financial statement analysis. More information availible at: https://github.com/TimoKats/fibooks/',
+    install_requires=['openpyxl','pandas','setuptools'],
+    keywords=['finance', 'data','valuation','excel']
 )
